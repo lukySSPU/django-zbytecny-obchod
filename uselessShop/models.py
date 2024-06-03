6c524b02903515170fc505aef1c65b2b5bce03c6
LukySSPU <98309051+lukySSPU@users.noreply.github.com>
2024-06-03 11:29:07 +0200

    Update models.py

--- uselessShop/models.py
+++ uselessShop/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+class Comment(models.Model):
+    username = models.CharField(max_length=30, default="anonymous")
+    text = models.TextField(default="...", blank=True)
+
 class Item(models.Model):
     img = models.ImageField(upload_to="images/", default=None, blank=True)
     name = models.CharField(max_length=30)
     desc = models.TextField()
     price = models.FloatField()
+    comment = models.ManyToManyField(Comment, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}"
